693546e904b569db0c197ae87ce51656ec72337b
jsm <jsm@fotoware.com>
2013-10-05 23:46:11 +0200

    meh

--- backend/app.py
+++ backend/app.py
@@ -13,19 +13,6 @@ from guardian import search_guardian
 app = Flask(__name__)
 
 
-text = """
-Foreign military forces appear to have carried out a pre-dawn raid on a southern Somalian coastal town, apparently in pursuit of "a high-profile target" linked to the militant al-Shabaab group that was behind last month's Kenyan mall shootings.
-
-The pre-dawn raid – which initial but unconfirmed reports suggested may have involved US troops – took place in Barawe, in the lower Shabelle region 240km south of Mogadishu. It is the same town where US navy commandos killed a senior al-Qaida member four years ago.
-
-The raid comes as Kenya's military confirmed the names of four al-Shabaab fighters implicated in the Westgate attack. Major Emmanuel Chirchir said the men were Abu Baara al-Sudani, Omar Nabhan, Khattab al-Kene and Umayr – names that were first broadcast by a local Kenyan television station.
-
-"I confirm those are the names of the terrorist," he said, in a tweet sent to the Associated Press.
-
-The publication of the identities supports CCTV footage from the Nairobi mall published by a private TV station that shows no more than four attackers, contradicting earlier government statements that between 10 to 15 attackers were involved.
-"""
-
-
 # Generator for searching in storyful API
 def storyful(name):
     for story in search_storyful(name):
