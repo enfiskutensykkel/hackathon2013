e8586ffb4c2a27cc3408e9dc3c228163c2f34c66
shadowano <mh82@online.no>
2013-10-06 01:02:52 +0200

    me

--- backend/static/template.js
+++ backend/static/template.js
@@ -1,4 +1,4 @@
 
-var qItem = '{{each data}}<div class="quoteItem"><div class="date quotePart">${date} - ${source}</div><div class="quote quotePart">${quote}</div><div class="quotePart">said <span class="who">${who}</span></div><div class="entity quotePart"><p>tags: ${tags}</div></div>{{/each}}';
+var qItem = '{{each data}}<div class="quoteItem"><div class="date quotePart"><a href="">${date} - ${source}</a></div><div class="quote quotePart">"${quote}", </div><div class="quotePart">said <span class="who">${who}</span></div><div class="entity quotePart"><p>tags: ${tags}</div></div>{{/each}}';
 
 $.template( "quoteItemTmpl", qItem );
